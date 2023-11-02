317614eb880bf5ff012e12c3e2c77cdb2ce8534d
dogmania <hj296914@gmail.com>
2023-11-03 01:22:35 +0900

    :sparkles: Feat: 일기 분석 프래그먼트 구현

--- app/src/main/java/com/example/snack4diet/analysis/DiaryAnalysisFragment.kt
+++ app/src/main/java/com/example/snack4diet/analysis/DiaryAnalysisFragment.kt
@@ -1,60 +1,142 @@
 package com.example.snack4diet.analysis
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import com.example.snack4diet.R
+import com.example.snack4diet.databinding.FragmentDiaryAnalysisBinding
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.ChartData
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DiaryAnalysisFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DiaryAnalysisFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentDiaryAnalysisBinding
+    private lateinit var weekLineChart: LineChart
+    private lateinit var fourWeekLineChart: LineChart
+    private lateinit var buttons: List<ImageButton>
+    private val weekChartData = arrayListOf<Entry>()
+    private val fourWeekChartData = arrayListOf<Entry>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary_analysis, container, false)
+        binding = FragmentDiaryAnalysisBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DiaryAnalysisFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DiaryAnalysisFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttons = listOf(
+            binding.btnKcal,
+            binding.btnCarbohydrate,
+            binding.btnProtein,
+            binding.btnProvince
+        )
+
+        for (i in buttons.indices) {
+            buttons[i].setOnClickListener {
+                selectButton(i)
             }
+        }
+
+        weekChartData.add(Entry(10.0f, 30.0f))
+        weekChartData.add(Entry(11.0f, 35.0f))
+        weekChartData.add(Entry(12.0f, 39.0f))
+        weekChartData.add(Entry(13.0f, 31.0f))
+        weekChartData.add(Entry(14.0f, 36.0f))
+        weekChartData.add(Entry(15.0f, 34.0f))
+        weekChartData.add(Entry(16.0f, 30.0f))
+
+        fourWeekChartData.add(Entry(10.0f, 34.0f))
+        fourWeekChartData.add(Entry(11.0f, 39.0f))
+        fourWeekChartData.add(Entry(12.0f, 36.0f))
+        fourWeekChartData.add(Entry(13.0f, 33.0f))
+        fourWeekChartData.add(Entry(14.0f, 31.0f))
+        fourWeekChartData.add(Entry(15.0f, 38.0f))
+        fourWeekChartData.add(Entry(16.0f, 35.0f))
+
+        weekLineChart(weekChartData)
+        fourWeekLineChart(fourWeekChartData)
+    }
+
+    private fun weekLineChart(chartData: ArrayList<Entry>) {
+        weekLineChart = binding.weekLineChart
+
+        val lineDataSet = LineDataSet(chartData, "최근 7일간")
+
+        weekLineChart.apply {
+            data = LineData(lineDataSet)
+            invalidate()
+        }
+
+        weekLineChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM   //x축의 표시를 정하는 것. 기본값은 TOP이다.
+        }
+
+        lineDataSet.apply {
+            lineWidth = 3f
+            circleRadius = 5f
+            color = resources.getColor(R.color.orange)
+            valueTextSize = 15f
+            circleHoleColor = resources.getColor(R.color.orange)
+            circleColors = mutableListOf(resources.getColor(R.color.orange))
+        }
+    }
+
+    private fun fourWeekLineChart(chartData: ArrayList<Entry>) {
+        fourWeekLineChart = binding.fourWeekLineChart
+
+        val lineDataSet = LineDataSet(chartData, "최근 4주간")
+
+        fourWeekLineChart.apply {
+            data = LineData(lineDataSet)
+            invalidate()
+        }
+
+        fourWeekLineChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM   //x축의 표시를 정하는 것. 기본값은 TOP이다.
+        }
+
+        lineDataSet.apply {
+            lineWidth = 3f
+            circleRadius = 5f
+            color = resources.getColor(R.color.orange)
+            valueTextSize = 15f
+            circleHoleColor = resources.getColor(R.color.orange)
+            circleColors = mutableListOf(resources.getColor(R.color.orange))
+        }
+    }
+
+    private fun selectButton(i: Int) {
+        buttons[0].setImageResource(R.drawable.ic_btn_kcal_unchecked)
+        buttons[1].setImageResource(R.drawable.ic_btn_carbohydrate_unchecked)
+        buttons[2].setImageResource(R.drawable.ic_btn_protein_unchecked)
+        buttons[3].setImageResource(R.drawable.ic_btn_province_unchecked)
+
+        when(i) {
+            0 -> {
+                buttons[0].setImageResource(R.drawable.ic_btn_kcal_checked)
+            }
+            1 -> {
+                buttons[1].setImageResource(R.drawable.ic_btn_carbohydrate_checked)
+            }
+            2 -> {
+                buttons[2].setImageResource(R.drawable.ic_btn_protein_checked)
+            }
+            3 -> {
+                buttons[3].setImageResource(R.drawable.ic_btn_province_checked)
+            }
+        }
     }
 }
