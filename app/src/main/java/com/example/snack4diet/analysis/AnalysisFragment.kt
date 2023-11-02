07c29387355f35ceb641d05ef628019a6fadad2f
dogmania <hj296914@gmail.com>
2023-11-03 01:20:48 +0900

    :sparkles: Feat: 일기 분석 프래그먼트로 전환하는 함수 구현

--- app/src/main/java/com/example/snack4diet/analysis/AnalysisFragment.kt
+++ app/src/main/java/com/example/snack4diet/analysis/AnalysisFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.snack4diet.R
 import com.example.snack4diet.databinding.FragmentAnalysisBinding
 
@@ -23,6 +24,22 @@ class AnalysisFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnDiaryAnalysis.setTextColor(ContextCompat.getColor(requireContext(), R.color.orange))
+        binding.underLine2.visibility = View.GONE
 
+        setDiaryAnalysisFragment()
+    }
+
+    private fun setDiaryAnalysisFragment() {
+        val diaryAnalysisFragment = DiaryAnalysisFragment()
+
+        replaceChildFragment(diaryAnalysisFragment, "diaryAnalysisFragment")
+    }
+
+    private fun replaceChildFragment(fragment: Fragment, tag: String) {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.subFrame, fragment, tag)
+            .addToBackStack(null) // 이전 프래그먼트를 백스택에 추가
+            .commit()
     }
 }
