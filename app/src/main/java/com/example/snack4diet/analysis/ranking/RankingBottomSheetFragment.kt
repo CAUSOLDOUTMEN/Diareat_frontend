1ca6fd8cee3fe06ebd5cd0d8aa70d07474b5238a
dogmania <hj296914@gmail.com>
2023-11-04 20:05:10 +0900

    :sparkles: Feat: 주간 랭킹 바텀시트 구현

--- app/src/main/java/com/example/snack4diet/analysis/ranking/RankingBottomSheetFragment.kt
+++ app/src/main/java/com/example/snack4diet/analysis/ranking/RankingBottomSheetFragment.kt
@@ -5,56 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.snack4diet.MainActivity
 import com.example.snack4diet.R
+import com.example.snack4diet.api.UserRank
+import com.example.snack4diet.databinding.FragmentWeeklyRankingBottomSheetBinding
+import com.example.snack4diet.viewModel.NutrientsViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RankingBottomSheetFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class RankingBottomSheetFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class RankingBottomSheetFragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentWeeklyRankingBottomSheetBinding
+    private lateinit var viewModel: NutrientsViewModel
+    private lateinit var followings: List<UserRank>
+    private var position = -1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ranking_bottom_sheet, container, false)
+        binding = FragmentWeeklyRankingBottomSheetBinding.inflate(layoutInflater, container, false)
+
+        position = arguments?.getInt("position", -1)!!
+        viewModel = (requireActivity() as MainActivity).getViewModel()
+        viewModel.followingLiveData.observe(requireActivity()) {following ->
+            followings = following
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RankingBottomSheetFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RankingBottomSheetFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ranking.text = (position + 1).toString() + "위"
+        binding.userName.text = followings[position].name
+        binding.kcal.text = followings[position].kcalScore.toString()
+        binding.carbohydrate.text = followings[position].carbohydrateScore.toString()
+        binding.protein.text = followings[position].proteinScore.toString()
+        binding.province.text = followings[position].provinceScore.toString()
+        binding.totalScore.text = followings[position].totalScore.toString()
     }
 }
