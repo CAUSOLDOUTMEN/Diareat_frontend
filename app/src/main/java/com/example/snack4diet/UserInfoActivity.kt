88764bc64ca324d8b5ed81ece800e0379c5ee006
dogmania <hj296914@gmail.com>
2023-11-10 11:26:40 +0900

    :sparkles: Feat: api 연결

--- app/src/main/java/com/example/snack4diet/UserInfoActivity.kt
+++ app/src/main/java/com/example/snack4diet/UserInfoActivity.kt
@@ -1,25 +1,41 @@
 package com.example.snack4diet
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.RadioGroup
 import android.widget.Toast
+import com.example.snack4diet.api.Join
 import com.example.snack4diet.api.UserInfo
+import com.example.snack4diet.application.MyApplication
 import com.example.snack4diet.databinding.ActivityUserInfoBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class UserInfoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUserInfoBinding
     private lateinit var radioGroup: RadioGroup
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var app: MyApplication
+    private var accessToken = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        app = applicationContext as MyApplication
+        sharedPreferences = getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE)
         radioGroup = binding.sex
+
+        accessToken = intent.getStringExtra("accessToken")!!
 
         val textWatcher = object: TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -56,11 +72,28 @@ class UserInfoActivity : AppCompatActivity() {
 
     private fun saveUserInfo() {
         val nickname = binding.nickname.text.toString()
-        val sex: Boolean = radioGroup.checkedRadioButtonId == R.id.male // false이면 여성 true이면 남성
-        val height = binding.height.text.toString().toDouble()
-        val weight = binding.weight.text.toString().toDouble()
+        var gender = 0
+        val height = binding.height.text.toString().toInt()
+        val weight = binding.weight.text.toString().toInt()
         val age = binding.age.text.toString().toInt()
-        val user = UserInfo(nickname , height, weight, sex, age)
+        val app = applicationContext as MyApplication
+
+        if (radioGroup.checkedRadioButtonId == R.id.male) gender = 1
+        else if (radioGroup.checkedRadioButtonId == R.id.female) gender = 0
+
+        val user = Join(age, gender, height, nickname, accessToken, weight)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = app.apiService.joinUser(user)
+                sharedPreferences.edit().putLong("id", response.data.id!!).apply()
+                sharedPreferences.edit().putString("jwt", response.data.jwt).apply()
+                val intent = Intent(this@UserInfoActivity, MainActivity::class.java)
+                startActivity(intent)
+            } catch (e: Exception) {
+                Log.e("UserInfoActivity", "Error during joinUser API call", e)
+            }
+        }
 
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
