d07c84b14769dd41353fab79e2372f575fadc690
dogmania <hj296914@gmail.com>
2023-11-12 18:36:07 +0900

    :recycle: Refactor: foodOnDate API data class 수정

--- app/src/main/java/com/example/snack4diet/api/foodOnDate/Data.kt
+++ app/src/main/java/com/example/snack4diet/api/foodOnDate/Data.kt
@@ -2,10 +2,8 @@ package com.example.snack4diet.api.foodOnDate
 
 data class Data(
     val baseNutrition: BaseNutrition,
-    val date: String,
     val favorite: Boolean,
     val foodId: Int,
     val name: String,
-    val time: Time,
     val userId: Int
 )
