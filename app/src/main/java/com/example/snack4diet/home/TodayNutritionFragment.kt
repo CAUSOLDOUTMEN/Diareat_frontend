d7688b2325888bf7e78d655e5c36ed3d8d6246ab
dogmania <hj296914@gmail.com>
2023-11-12 18:42:05 +0900

    :recycle: Refactor: HomeFragment로부터 분리 및 API 연결

--- app/src/main/java/com/example/snack4diet/home/TodayNutritionFragment.kt
+++ app/src/main/java/com/example/snack4diet/home/TodayNutritionFragment.kt
@@ -1,60 +1,55 @@
 package com.example.snack4diet.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.snack4diet.R
+import com.example.snack4diet.api.nutritionSummary.NutritionSummary
+import com.example.snack4diet.databinding.FragmentTodayNutritionBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TodayNutritionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TodayNutritionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentTodayNutritionBinding
+    private lateinit var dailyNutrition: NutritionSummary
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today_nutrition, container, false)
+        binding = FragmentTodayNutritionBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TodayNutritionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TodayNutritionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragment = requireParentFragment() as HomeFragment
+        dailyNutrition = fragment.getDailyNutrition()
+        Log.e("ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ", dailyNutrition.toString())
+
+        binding.dailyKcal.text = dailyNutrition.data.totalKcal.toString() + "kcal / " + dailyNutrition.data.baseNutrition.kcal.toString() + "kcal"
+        binding.dailyCarbohydrate.text = dailyNutrition.data.totalCarbohydrate.toString() + "g / " + dailyNutrition.data.baseNutrition.carbohydrate.toString() + "g"
+        binding.dailyProtein.text = dailyNutrition.data.totalProtein.toString() + "g / " + dailyNutrition.data.baseNutrition.protein.toString() + "g"
+        binding.dailyFat.text = dailyNutrition.data.totalFat.toString() + "g / " + dailyNutrition.data.baseNutrition.fat.toString() + "g"
+
+        binding.percentKcal.text = dailyNutrition.data.ratioKcal.toString() + "%"
+        binding.percentCarbohydrate.text = dailyNutrition.data.ratioCarbohydrate.toString() + "%"
+        binding.percentProtein.text = dailyNutrition.data.ratioProtein.toString() + "%"
+        binding.percentFat.text = dailyNutrition.data.ratioFat.toString() + "%"
+
+        if (dailyNutrition.data.ratioKcal > 100.0) {
+            binding.percentKcal.setTextColor(ContextCompat.getColor(requireContext(), R.color.red))
+        }
+        if (dailyNutrition.data.ratioCarbohydrate > 100.0) {
+            binding.percentCarbohydrate.setTextColor(ContextCompat.getColor(requireContext(), R.color.red))
+        }
+        if (dailyNutrition.data.ratioProtein > 100.0) {
+            binding.percentProtein.setTextColor(ContextCompat.getColor(requireContext(), R.color.red))
+        }
+        if (dailyNutrition.data.ratioFat > 100.0) {
+            binding.percentFat.setTextColor(ContextCompat.getColor(requireContext(), R.color.red))
+        }
     }
 }
