4532299478a1567151a8562982f55340f60b55f8
dogmania <hj296914@gmail.com>
2023-11-19 00:00:04 +0900

    :recycle: Refactor: 검색 어댑터 수정

--- app/src/main/java/com/example/snack4diet/analysis/ranking/SearchAdapter.kt
+++ app/src/main/java/com/example/snack4diet/analysis/ranking/SearchAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.snack4diet.analysis.ranking
 
+import android.content.Context
 import android.graphics.Color
 import android.graphics.Typeface
 import android.net.Uri
@@ -7,16 +8,28 @@ import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.snack4diet.R
 import com.example.snack4diet.api.UserRank
 import com.example.snack4diet.api.UserSearch
+import com.example.snack4diet.api.searchUserResponse.Data
 import com.example.snack4diet.databinding.ItemRankingBinding
 import com.example.snack4diet.databinding.ItemSearchBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class SearchAdapter(private var searchResult: List<UserSearch>, private val searchedTitle: String): RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
+class SearchAdapter(
+    private val context: Context,
+    private var searchResult: List<Data>,
+    private val searchedTitle: String,
+    private val followListener: WeeklyRankingFragment.FollowListener
+): RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
 
     inner class ViewHolder(binding: ItemSearchBinding): RecyclerView.ViewHolder(binding.root) {
         val ranking = binding.ranking
@@ -36,22 +49,55 @@ class SearchAdapter(private var searchResult: List<UserSearch>, private val sear
 
         holder.ranking.text = (position + 1).toString()
         if (item.image != null) {
-            holder.profileImage.setImageURI(Uri.parse(item.image))
+            Glide.with(context)
+                .load(item.image)
+                .placeholder(R.drawable.ic_profile_image)
+                .circleCrop()
+                .into(holder.profileImage)
         }
         holder.nickname.text = item.name
         holder.btnFollow.isChecked = item.follow
+//        holder.btnFollow.setOnClickListener {
+//            item.follow = holder.btnFollow.isChecked
+//            if (item.follow) {
+//                followListener.followUser(item.userId)
+//            } else {
+//                followListener.unfollowUser(item.userId)
+//            }
+//        }
+
         holder.btnFollow.setOnClickListener {
-            item.follow = holder.btnFollow.isChecked
+            val isChecked = holder.btnFollow.isChecked
+            holder.btnFollow.isChecked = !isChecked // UI를 우선 업데이트
+
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    if (isChecked) {
+                        followListener.unfollowUser(item.userId)
+                    } else {
+                        followListener.followUser(item.userId)
+                    }
+                    withContext(Dispatchers.Main) {
+                        // 팔로우 상태가 성공적으로 변경된 경우 UI 업데이트
+                        item.follow = isChecked
+                        holder.btnFollow.isChecked = isChecked
+                    }
+                } catch (e: Exception) {
+                    Log.e("YourAdapter", "Error during follow/unfollow API call", e)
+                    withContext(Dispatchers.Main) {
+                        // 실패 시 다시 원래 상태로 돌려놓기
+                        holder.btnFollow.isChecked = !isChecked
+                    }
+                }
+            }
         }
+
+
         holder.nickname.text = highlightKeyword(item.name, searchedTitle)
     }
 
     override fun getItemCount(): Int {
         return searchResult.size
-    }
-
-    fun updateData(newData: List<UserSearch>) {
-        searchResult = newData
     }
 
     private fun highlightKeyword(text: String, keyword: String): SpannableStringBuilder {
