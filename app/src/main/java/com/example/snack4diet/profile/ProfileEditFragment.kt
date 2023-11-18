e71d38e2de66ebbcf63f26bcc52f74c47ce29219
dogmania <hj296914@gmail.com>
2023-11-19 00:13:24 +0900

    :sparkles: Feat: 유저 정보 수정 api 연결

--- app/src/main/java/com/example/snack4diet/profile/ProfileEditFragment.kt
+++ app/src/main/java/com/example/snack4diet/profile/ProfileEditFragment.kt
@@ -7,24 +7,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.example.snack4diet.MainActivity
 import com.example.snack4diet.R
-import com.example.snack4diet.api.UserInfo
-import com.example.snack4diet.databinding.AddDiaryDialogLayoutBinding
+import com.example.snack4diet.api.updateUser.UpdateUserDto
 import com.example.snack4diet.databinding.DialogProfileEditExceptionBinding
 import com.example.snack4diet.databinding.FragmentProfileEditBinding
-import com.example.snack4diet.viewModel.NutrientsViewModel
+import kotlinx.coroutines.launch
 
 class ProfileEditFragment : Fragment() {
     private lateinit var binding: FragmentProfileEditBinding
-    private lateinit var viewModel: NutrientsViewModel
-    private lateinit var user: UserInfo
+    private lateinit var mainActivity: MainActivity
+    private var userInfo: com.example.snack4diet.api.userInfo.UserInfo? = null
+    private var userId = -1L
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainActivity = requireActivity() as MainActivity
+        userId = mainActivity.getUserId()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentProfileEditBinding.inflate(layoutInflater, container, false)
+
+        lifecycleScope.launch{
+            userInfo = mainActivity.getUserInfo()
+            binding.nickname.hint = userInfo?.data?.name
+            binding.age.hint = userInfo?.data?.age.toString()
+            binding.height.hint = userInfo?.data?.height.toString()
+            binding.weight.hint = userInfo?.data?.weight.toString()
+        }
 
         return binding.root
     }
@@ -32,30 +47,29 @@ class ProfileEditFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val mainActivity = requireActivity() as MainActivity
-        viewModel = mainActivity.getViewModel()
-        user = viewModel.getUser()
-
-        binding.nickname.hint = user.nickname
-        binding.age.hint = user.age.toString()
-        binding.height.hint = user.height.toInt().toString()
-        binding.weight.hint = user.weight.toInt().toString()
-
         binding.btnFinishEdit.setOnClickListener {
             var newName = binding.nickname.text.toString()
             var newAge = binding.age.text.toString()
             var newHeight = binding.height.text.toString()
             var newWeight = binding.weight.text.toString()
 
-            if (newName.isNullOrEmpty()) newName = user.nickname
-            if (newAge.isNullOrEmpty()) newAge = user.age.toString()
-            if (newHeight.isNullOrEmpty()) newHeight = user.height.toString()
-            if (newWeight.isNullOrEmpty()) newWeight = user.weight.toString()
+            if (newName.isEmpty()) newName = binding.nickname.hint.toString()
+            if (newAge.isEmpty()) newAge = binding.age.hint.toString()
+            if (newHeight.isEmpty()) newHeight = binding.height.hint.toString()
+            if (newWeight.isEmpty()) newWeight = binding.weight.hint.toString()
 
-            if (newHeight.toInt() > 300 || newAge.toInt() > 120 || newWeight.toInt() > 200) {
+            val parsedAge = newAge.toIntOrNull()
+            val parsedHeight = newHeight.toDoubleOrNull()
+            val parsedWeight = newWeight.toDoubleOrNull()
+
+            if (parsedHeight == null || parsedAge == null || parsedWeight == null ||
+                parsedHeight.toDouble() > 300.0 || parsedAge.toInt() > 120 || parsedWeight.toDouble() > 200.0) {
                 showProfileEditExceptionDialog()
             } else {
-                viewModel.editUser(newName, newHeight.toDouble(), newWeight.toDouble(), newAge.toInt())
+                val newUserInfo = UpdateUserDto(
+                     parsedAge, parsedHeight, newName, userId, parsedWeight
+                )
+                mainActivity.updateUserInfo(newUserInfo)
                 mainActivity.popFragment()
                 Toast.makeText(requireContext(),"수정이 완료되었습니다.",
                     Toast.LENGTH_SHORT).show()
